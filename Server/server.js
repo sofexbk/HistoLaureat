03174b3f57cffd0de3f8f702b15ddbb642711be0
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-12-01 00:06:32 +0100

    fix server config

---
+++ Server/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express=require('express')
+const mongoose=require('mongoose')
+const bodyParser = require('body-parser');
+//const userRoutes=require('./routes/user')
+
+const app=express()
+app.use(bodyParser.json());
+
+app.use(express.json())
+
+app.use((req,res,next)=>{ 
+    console.log(req.path,req.method)
+    next()
+});
+//app.use('/api/user',userRoutes)
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+   console.log('Connected To db && listening on port',process.env.PORT)
+})
+})
+.catch((error)=>{
+   console.log(error)
+})
+
