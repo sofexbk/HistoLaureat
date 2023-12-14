bba90b235b2583bc8a93be1a93f00a8b559b7c98
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-12-14 12:11:09 +0100

    delete client

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -19,17 +19,18 @@ const loginUser=async (req,res)=>{
     res.status(400).json({error:error.message})
 }}
 
-const signupUser=async (req,res)=>{
-    const {email,password,role,confirmPassword}=req.body
+const signupUser = async (req, res) => {
+    const { email, password, role, confirmPassword } = req.body;
     console.log('Request Body:', req.body);
-    try{
+    try {
         const user = await User.signup(email, password, confirmPassword, role);
-        const token=createToken(user._id)
-        res.status(200).json({email,token,role})
-    }catch (error) {
-        res.status(400).json({error:error.message})
-}}
-
+        const profile = await UserProfile.create({ userId: user._id, /* other profile fields */ });
+        const token = createToken(user._id, profile._id);
+        res.status(200).json({ email, token, role });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
 module.exports={
     signupUser,loginUser
 }
--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -31,6 +31,30 @@ const getUserIdFromToken = (token) => {
       res.status(401).json({ message: 'Unauthorized' });
     }
   };
+
+  /*
+  const verifyToken = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Authorization token is missing.' });
+        }
+        const { userId, profileId } = jwt.verify(token, process.env.SECRET);
+
+        // Vérifiez que le Profil ID dans le token correspond au Profil ID associé à l'utilisateur
+        const userProfile = await Profile.findOne({ userId, _id: profileId });
+        if (!userProfile) {
+            return res.status(401).json({ message: 'Invalid or expired token.' });
+        }
+
+        req.userId = userId;
+        req.profileId = profileId;
+        next();
+    } catch (error) {
+        console.error('Error verifying token:', error.message);
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+}; */
   
   const authMiddleware = async (req, res, next) => {
     try {
