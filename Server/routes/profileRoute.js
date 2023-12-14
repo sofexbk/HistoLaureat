3001c29fc58403dab3faaa85bfc4e6311b5573e4
MOHAMMED YASSINE EL AALOUCH <elaalouch.dev@gmail.com>
2023-12-14 09:23:06 +0100

    setup client project

--- Server/routes/profileRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profileController');
-const authMiddleware = require('../middlewares/auth');
-router.post('/createProfile', authMiddleware.verifyToken, profileController.createProfile);
-router.patch('/updateProfile', authMiddleware.verifyToken, profileController.updateProfile);
-
-module.exports = router;
