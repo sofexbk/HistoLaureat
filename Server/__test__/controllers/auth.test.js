9196a10d77884adc1ba268db31280a89b0967e9f
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-12-16 12:55:02 +0100

    test signup

--- Server/__test__/controllers/auth.test.js
+++ Server/__test__/controllers/auth.test.js
@@ -1,4 +1,4 @@
-const { loginUser } = require('../../controllers/userController.js');
+const { signupUser,loginUser } = require('../../controllers/userController.js');
 const User = require('../../models/userModel');
 const jwt = require('jsonwebtoken');
 
@@ -7,7 +7,9 @@ jest.mock('jsonwebtoken', () => ({
 }));
 jest.mock('../../models/userModel', () => ({
   login: jest.fn(),
+  signup: jest.fn()
 }));
+
 describe('loginUser', () => {
   it('should successfully log in a user with valid email and password', async () => {
     const req = {
@@ -25,6 +27,30 @@ describe('loginUser', () => {
     User.login.mockResolvedValueOnce(mockUser);
     await loginUser(req, res);
     expect(User.login).toHaveBeenCalledWith('test@example.com', 'password123');
+    expect(jwt.sign).toHaveBeenCalledWith({ _id: '123' }, process.env.SECRET, { expiresIn: '50d' });
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ email: 'test@example.com', token: 'token123', role: 'user' });
+  });
+});
+describe('signupUser', () => {
+  it('should successfully sign up a user with valid email, password, role, and confirmPassword', async () => {
+    const req = {
+      body: {
+        email: 'test@example.com',
+        password: 'password123',
+        role: 'user',
+        confirmPassword: 'password123'
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    const mockUser = { _id: '123', role: 'user' };
+    jwt.sign.mockReturnValueOnce('token123');
+    User.signup.mockResolvedValueOnce(mockUser);
+    await signupUser(req, res);
+    expect(User.signup).toHaveBeenCalledWith('test@example.com', 'password123', 'password123', 'user');
     expect(jwt.sign).toHaveBeenCalledWith({ _id: '123' }, process.env.SECRET, { expiresIn: '50d' });
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith({ email: 'test@example.com', token: 'token123', role: 'user' });
