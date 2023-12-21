080b0e9a902679ac63de04c1cb0ad0e25bfa63b7
MOHAMMED YASSINE EL AALOUCH <elaalouch.dev@gmail.com>
2023-12-21 22:04:01 +0100

    merged create-profile figma code

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,27 @@ import Signup from './pages/Signup';
 import { useAuthContext } from './hooks/useAuthContext';
 import Home from './components/Home';
 import CreateProfile from './components/CreateProfile';
+import Landing from './components/Landing';
+import NewPost from './components/NewPost';
+import NewStage from './components/NewStage';
+import Navbar from './components/Navbar'
+
 
 function App() {
   const {user}=useAuthContext();
   return (
     <Router>
       <div className="App">
+            <Navbar></Navbar>
           <Routes>
-          <Route path="/login" element={!user ? <Login/> : <Navigate to="/home" />} />
+            <Route path="/login" element={!user ? <Login/> : <Navigate to="/home" />} />
             <Route path="/signup" element={!user ? <Signup/> : <Navigate to="/home" />} />
             <Route  path="/home" element={user ? <Home/>: <Navigate to="/login" /> } />
             <Route  path="/create-profile" element={ <CreateProfile/>}/>
+            <Route path="/landing" element={<Landing/>} />
+            <Route path='/new-post' element={<NewPost/>}/>
+            <Route path='/new-stage' element={<NewStage/>}/>
+
           </Routes>
       </div>
     </Router>
