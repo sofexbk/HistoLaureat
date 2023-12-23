a3861d56eef0cee787d7aa5ab29e2099c47c4cd2
EL MOUMOUHI Houda <houda.elmoumouhi@gmail.com>
2023-12-23 10:08:50 +0100

    createPost_Stage

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Signup from './pages/Signup';
 import { useAuthContext } from './hooks/useAuthContext';
 import Home from './components/Home';
 import CreateProfile from './components/CreateProfile';
+import CreatePost from './components/CreatePost';
+import CreateStage from './components/CreateStage';
 
 function App() {
   const {user}=useAuthContext();
@@ -15,8 +17,13 @@ function App() {
           <Routes>
           <Route path="/login" element={!user ? <Login/> : <Navigate to="/home" />} />
             <Route path="/signup" element={!user ? <Signup/> : <Navigate to="/home" />} />
-            <Route  path="/home" element={user ? <Home/>: <Navigate to="/login" /> } />
+            {//<Route  path="/home" element={user ? <Home/>: <Navigate to="/login" /> } />
+}
+            <Route  path="/home" element={ <Home/>  }/>
             <Route  path="/create-profile" element={ <CreateProfile/>}/>
+            <Route path='/create-post' element={ <CreatePost/>} />
+            <Route  path="/create-profile" element={ <CreateProfile/>}/>
+            <Route  path="/create-stage" element={ <CreateStage/>}/>
           </Routes>
       </div>
     </Router>
